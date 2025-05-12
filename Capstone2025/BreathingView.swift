9f64ee45b60ea2a3471dfe088e4e1a06ab3e68a8
Shira Stein <sstein@jkha.org>
2025-05-11 21:02:46 -0400

    added padding

--- Capstone2025/BreathingView.swift
+++ Capstone2025/BreathingView.swift
@@ -12,13 +12,19 @@ import SwiftUI
 struct BreathingView: View {
     var body: some View {
         Text("Focus on your breathing 💨 and heartbeat 💓.")
+            .font(.title)
+            .fontWeight(.heavy)
             .foregroundColor(Color.purple)
+            .multilineTextAlignment(.center)
             .padding()
             .navigationTitle("Breathing")
         
         Link("5 Minute Meditation", destination: URL(string: "https://www.youtube.com/watch?v=XBULNfOFfAA")!)
+            .padding(.all)
         Link("Calming Coloring Sheets", destination: URL(string: "https://iheartcraftythings.com/stress-relief-coloring-pages.html")!)
+            .padding(.all)
         Link("15 Minute Stress Relief Yoga", destination: URL(string: "https://www.youtube.com/watch?v=Nw2oBIrQGLo")!)
+            .padding(.all)
 
         Image ("breathing")
     }
